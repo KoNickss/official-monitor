92f726906cdb6ab5e1e851a50030dd8a0c9706ce
KoNicks <konicksdev@gmail.com>
2021-04-16 22:38:04 +0300

    added quickview

--- build.sh
+++ build.sh
@@ -1,14 +1,3 @@
 #!/bin/bash
 php index.php > index.html
-cd laws
-for folder in $(ls -d */)
-do
-	cd $folder
-	cp ../../folder.php ./index.php
-	php index.php > index.html
-	rm index.php
-	cd ..
-	echo "--------------------------------- PHP ---------------------------------------------"
-	echo "Done converting $folder to bootstrap"
-	echo "-----------------------------------------------------------------------------------"
-done
+echo "done."
