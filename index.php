954fadcdec9753a1b0819865e8e6963155e4685d
KoNicks <konicksdev@gmail.com>
2021-05-13 00:56:01 +0300

    fixed NLA namechange

--- index.php
+++ index.php
@@ -220,8 +220,8 @@
 		<div id="Bucharest" class="tabcontent" style="background-color: white;">
             <div id="partybox" style="margin-left: 150px; margin-right: 150px;">
             <img src=nla.png height=100px>
-			<p class="infot" style="font-family: SF; font-size: 50px; text-decoration: none; font-weight: bold;">National Libertarian Alliance</p>
-            <p class="info" style="font-family: SF; font-size: 30px; margin-top: 0.5em; margin-bottom: 0em; font-weight: 400;">The National Libertarian Alliance will
+			<p class="infot" style="font-family: SF; font-size: 50px; text-decoration: none; font-weight: bold;">National Liberal Alliance</p>
+            <p class="info" style="font-family: SF; font-size: 30px; margin-top: 0.5em; margin-bottom: 0em; font-weight: 400;">The National Liberal Alliance will
                 pursue classical-liberal and libertarian 
                 policy, based on individual
                 freedoms, national identity and
